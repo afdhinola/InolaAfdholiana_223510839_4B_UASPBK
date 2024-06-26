4b3792d20cddac89706d9a371cff47b8894f0618
Inola Afdholiana <156224232+afdhinola@users.noreply.github.com>
2024-06-26 15:19:45 +0700

    UAS TEORI PBK

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,12 +1,18 @@
 <template>
     <div id="home">
+        <TodoListView />
         <router-view></router-view>
     </div>
 </template>
 
 <script>
+    import TodoListView from './TodoListView.vue';
+
     export default {
         name: 'HomeView',
+        components: {
+            TodoListView
+        }
     };
     
 </script>
